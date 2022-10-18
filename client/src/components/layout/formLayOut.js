018a8fc2b38edb12e99735cfffa450058b71cf70
harel858 <harellevi85@gmail.com>
2022-10-18 15:18:58 +0300

    finish project

--- client/src/components/layout/formLayOut.js
+++ client/src/components/layout/formLayOut.js
@@ -1,12 +1,7 @@
 import classes from "./style/formLayout.module.css";
-import NavBar from "../navbar/navbar";
+
 const body = document.querySelector(`body`);
 body.className = classes.body;
 export default function FormLayOut(props) {
-  return (
-    <>
-      <NavBar />
-      {props.children}
-    </>
-  );
+  return <main className={classes.mainForm}>{props.children}</main>;
 }
