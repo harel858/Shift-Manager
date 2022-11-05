9a1aa5c44eafb05a9e0ad325c5de19e69b79d6b3
harel858 <harellevi85@gmail.com>
2022-11-05 17:15:23 +0200

    dockerfile deleted

--- Backend/src/mongoose/connectToDB.js
+++ Backend/src/mongoose/connectToDB.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const dev_db_url = "mongodb://localhost/shiftManager";
+const dev_db_url =
+  "mongodb://mongo:4DEBvGroVWzlh4LLz5T6@containers-us-west-109.railway.app:7880";
 
 mongoose
   .connect(process.env.MONGODB_URI || dev_db_url)
