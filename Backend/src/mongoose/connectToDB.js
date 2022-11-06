975609b89f9f2601d23013e98c22477cea04c254
harel858 <harellevi85@gmail.com>
2022-11-06 13:32:14 +0200

    n

--- Backend/src/mongoose/connectToDB.js
+++ Backend/src/mongoose/connectToDB.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 console.log(process.env.MONGODB_URI);
-console.log(process.NODE_ENV.MONGODB_URI);
 console.log(process.env.NODE_ENV);
 mongoose
   .connect(process.env.MONGODB_URI || process.env.NODE_ENV)
