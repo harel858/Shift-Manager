4a856cbe37f9d7c59dde76696c22d453a38e14fc
harel858 <harellevi85@gmail.com>
2022-11-06 13:28:14 +0200

    b

--- Backend/src/mongoose/connectToDB.js
+++ Backend/src/mongoose/connectToDB.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
-
+console.log(process.env.MONGODB_URI);
+console.log(process.NODE_ENV.MONGODB_URI);
+console.log(process.env.NODE_ENV);
 mongoose
-  .connect(process.env.MONGODB_URI || process.NODE_ENV.MONGODB_URI)
+  .connect(process.env.MONGODB_URI || process.env.NODE_ENV)
   .then((x) => console.log("connect to DB successfuly"))
   .catch((e) => console.log(e));
