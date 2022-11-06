5d43b9707bbcf26173b52814546694877b663228
harel858 <harellevi85@gmail.com>
2022-11-06 10:56:18 +0200

    j

--- Backend/src/mongoose/connectToDB.js
+++ Backend/src/mongoose/connectToDB.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-console.log(process.env.MONGODB_URI);
+const MONGODB_URI =
+  "mongodb://mongo:4DEBvGroVWzlh4LLz5T6@containers-us-west-109.railway.app:7880/shiftManager";
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI || MONGODB_URI)
   .then((x) => console.log("connect to DB successfuly"))
   .catch((e) => console.log(e));
