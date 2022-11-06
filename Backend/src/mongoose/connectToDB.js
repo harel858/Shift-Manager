d59c1d0b7855ce6f26a552cc787c7a66d1b67eb1
harel858 <harellevi85@gmail.com>
2022-11-06 15:18:35 +0200

    tsitsi

--- Backend/src/mongoose/connectToDB.js
+++ Backend/src/mongoose/connectToDB.js
@@ -3,8 +3,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 console.log(process.env.MONGODB_URI);
 console.log(process.env.NODE_ENV);
-console.log(process.env.NODE_ENV.MONGODB_URI);
-console.log(process.env.NODE_ENV.MONGODB_URI);
+
 mongoose
   .connect(process.env.MONGODB_URI || process.env.NODE_ENV.MONGODB_URI)
   .then((x) => console.log("connect to DB successfully"))
