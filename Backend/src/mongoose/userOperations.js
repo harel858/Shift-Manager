ba77ef019f3d027a33f5cac0571a6c474280be68
harel858 <harellevi85@gmail.com>
2022-11-06 02:24:54 +0200

    err

--- Backend/src/mongoose/userOperations.js
+++ Backend/src/mongoose/userOperations.js
@@ -2,7 +2,11 @@ const userModel = require("./userModel");
 const validateUser = require("../joi/userValidation");
 
 async function addUser(userDetails) {
-  return await new userModel(userDetails).save();
+  try {
+    return await new userModel(userDetails).save();
+  } catch (err) {
+    return err;
+  }
 }
 async function updatePayment(id, payment) {
   return await userModel.findOneAndUpdate({ _id: id }, { $set: { payment } });
