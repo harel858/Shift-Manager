6890e4616d1145c4a090f050c15f42815d5c76d5
harel858 <harellevi85@gmail.com>
2022-11-13 12:17:40 +0200

    zz

--- Backend/src/mongoose/connectToDB.js
+++ Backend/src/mongoose/connectToDB.js
@@ -6,6 +6,11 @@ console.log(process.env.NODE_ENV);
 console.log("process.env");
 
 mongoose
-  .connect(process.env.MONGODB_URI || process.env.NODE_ENV.MONGODB_URI)
+  .connect(process.env.MONGODB_URI || process.env.NODE_ENV.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
   .then((x) => console.log("connect to DB successfully"))
   .catch((e) => console.log(e));
