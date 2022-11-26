3c34f1db10c7cf80469d727149bcbf9ce4c256b7
harel858 <harellevi85@gmail.com>
2022-11-26 12:53:37 +0200

    filtered

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -5,7 +5,6 @@ import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
 import { TextField } from "@mui/material";
 import UserContext from "../context/userContext.js";
-import CurrentShiftContext from "../context/currentShiftCtx.js";
 
 export default function Register() {
   const [userName, setUserName] = useState("");
@@ -17,7 +16,6 @@ export default function Register() {
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  /*  const { setCurrentShift } = useContext(CurrentShiftContext); */
   const { currency, payment, setLoginError, setUser, overTime } =
     useContext(UserContext);
 
@@ -50,7 +48,7 @@ export default function Register() {
         const user = await res.json();
         setError(null);
         setLoginError(null);
-        /*   setCurrentShift(null); */
+
         setUser({ ...user });
         navigate("/settings", { replace: true });
       } else {
